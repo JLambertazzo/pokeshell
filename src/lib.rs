41a5e2b4b73528bc6b234385255186a677d6ecc2
Julien <jbertazzolambert@gmail.com>
2022-04-17 11:06:47 -0400

    chore: nicer errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,24 @@
 use serde::Deserialize;
+use anyhow::{Result,Error,Context};
 
 #[derive(Debug,Deserialize)]
 #[allow(dead_code)]
 pub struct Pokemon {
-    number: i32,
-    name: String,
-    type1: String,
-    type2: String,
-    catch_rate: i32,
+    pub number: i32,
+    pub name: String,
+    pub type1: String,
+    pub type2: String,
+    pub catch_rate: i32,
 }
 
-pub fn find_pokemon(name: String) -> Result<Pokemon,String> {
+pub fn find_pokemon(name: String) -> Result<Pokemon> {
     let mut rdr = csv::Reader::from_path("./src/pokemon.csv")
-        .expect("error opening reader");
+        .with_context(||"error opening reader")?;
     for result in rdr.deserialize() {
-        let pokemon: Pokemon = result.expect("Could not read line");
+        let pokemon: Pokemon = result.with_context(||"Could not read line")?;
         if pokemon.name == name {
             return Ok(pokemon);
         }
     }
-    Err(String::from("Not found"))
+    Err(Error::msg("Pokemon not found"))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use pokeshell::find_pokemon;
+use anyhow::Result;
 
 /// CLI App for Pokemon Data
 #[derive(Parser)]
@@ -8,15 +9,22 @@ struct Cli {
     /// type of information to search [pokemon, move, info]
     #[clap(short,long,default_value="pokemon")]
     field: String,
+    /// attribute to search by see long about for options
+    #[clap(short,long,default_value="name")]
+    attr: String,
     /// What to search for
     query: String,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
     println!("Searching for the {}: `{}`", args.field, args.query);
     if args.field == "pokemon" {
-        let pokemon = find_pokemon(args.query).unwrap();
-        println!("Found: {:?}", pokemon);
+        let pokemon = find_pokemon(args.query);
+        match pokemon {
+            Ok(_) => println!("Found: {:?}", pokemon.unwrap()),
+            Err(e) => eprintln!("{}", e),
+        }
     }
+    Ok(())
 }
